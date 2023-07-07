61084329aa333169e7b713af111c5e6f12c203ab
crackun24 <53938436+crackun24@users.noreply.github.com>
2023-07-07 20:19:03 +0800

    队列储存玩家的操作

--- src/main/java/com/thestarryguard/thestarryguard/DataStorage.java
+++ src/main/java/com/thestarryguard/thestarryguard/DataStorage.java
@@ -1,14 +1,58 @@
 package com.thestarryguard.thestarryguard;
 
 
+import com.thestarryguard.thestarryguard.DataType.Action;
+import org.apache.commons.lang3.builder.ToStringExclude;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-enum DataBaseStorageType{MYSQL,SQL_LITE};//数据的储存使用的数据库类型
-public class DataStorage {//数据储存类
+import javax.xml.crypto.Data;
+import java.util.*;
+
+enum DataBaseStorageType {MYSQL, SQL_LITE};//数据的储存使用的数据库类型
+
+public class DataStorage extends Thread{//数据储存类,同时启动线程,不定时向数据库同步数据
+
     Logger LOGGER = LogManager.getLogger();//获取日志记录器
     private DataBaseStorageType mDbStorageType;//使用存储数据库的类型
 
+    private Queue<Action> mActionList;
+
+    private synchronized void PutActionToDb()//将玩家的行为存入数据库
+    {
+       this.mActionList.poll();//弹出数据
+    }
+
+    private DataStorage() {//构造函数
+        this.mActionList = new LinkedList<>();//实例化队列对象
+    }
+
+    public synchronized void PrintList(){//FIXME 测试方法
+        LOGGER.info("test");
+
+        Iterator<Action> iterator = this.mActionList.iterator();
+        while (iterator.hasNext())
+        {
+            Action temp = iterator.next();
+            LOGGER.info(temp.posX);
+        }
+    }
+
+    public synchronized void InsertAction(Action action) {//插入数据
+        this.mActionList.add(action);
+    }
+
+    public void run(){//数据库同步数据的线程
+        while (true)
+        {
+            try {
+                sleep(1000);
+                PrintList();//FIXME 调试
+                PutActionToDb();//弹出玩家的行为
+            } catch (InterruptedException e) {
+            }
+        }
+    }
     static public DataStorage GetDataStorage(final Config config) {//构造一个数据储存对象的工厂方法
         DataStorage temp_obj = new DataStorage();//示例化一个对象
 
@@ -16,7 +60,8 @@ public class DataStorage {//数据储存类
         {
             case "mysql":
                 temp_obj.mDbStorageType = DataBaseStorageType.MYSQL;//设置使用mysql存储数据
-               break;
+
+                break;
             case "sql_lite":
                 temp_obj.mDbStorageType = DataBaseStorageType.SQL_LITE;
                 break;
@@ -25,9 +70,5 @@ public class DataStorage {//数据储存类
         }
 
         return temp_obj;//返回构造好的对象
-    }
-
-    private DataStorage()//构造函数
-    {
     }
 }
--- src/main/java/com/thestarryguard/thestarryguard/TheStarryGuard.java
+++ src/main/java/com/thestarryguard/thestarryguard/TheStarryGuard.java
@@ -1,17 +1,21 @@
 package com.thestarryguard.thestarryguard;
 
+import net.minecraft.block.Block;
+import com.thestarryguard.thestarryguard.DataType.Action;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Identifier;
+import org.apache.commons.compress.compressors.lz77support.LZ77Compressor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.rmi.registry.Registry;
 
 
 public class TheStarryGuard implements ModInitializer {
@@ -23,6 +27,12 @@ public class TheStarryGuard implements ModInitializer {
 
     private void HookBreakEvent() {
         PlayerBlockBreakEvents.AFTER.register(((world, player, pos, state, blockEntity) -> {
+
+            String block_id = world.getBlockState(pos).getBlock().getName().getString();
+
+            Action action = new Action(Action.ActionType.BLOCK_BREAK, Action.TargetType.BLOCK, player.getUuidAsString(),
+                    block_id, pos.getX(), pos.getY(), pos.getZ(), Action.Dimension.OVER_WORLD, null);
+            this.mDataStorage.InsertAction(action);
 
         }));//方块破坏事件
     }//注册方块破坏事件
@@ -48,6 +58,7 @@ public class TheStarryGuard implements ModInitializer {
 
         LOGGER.info("Loading TheStarryGuard data storage.");
         this.mDataStorage = DataStorage.GetDataStorage(this.mConfig);//开始构建数据储存对象
+        this.mDataStorage.start();//启动数据同步线程
     }
 
     @Override
@@ -61,5 +72,6 @@ public class TheStarryGuard implements ModInitializer {
             throw new RuntimeException(e);//抛出异常,阻止服务器的启动
         }
 
+        HookBreakEvent();//FIXME
     }
 }
